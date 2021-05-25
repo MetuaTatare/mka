68d577bbffacba81f59b9eb67de42b4d1b850aad
MetuaTatare <Moresby_Isaacs@hotmail.co.nz>
2021-05-25 23:04:34 +1200

    Building Configuration files
    The configuration files are to allow the Corrella angular application to run

---
+++ libs/browse/components/src/buttons/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mka-button',
+    templateUrl: 'button.component.html',
+    styleUrls: ['button.scss'],
+})
+
+export class MKAButtonComponent {
+    
+}
